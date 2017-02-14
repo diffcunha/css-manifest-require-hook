b793b487c9a03c0517bf737d9b353362a283f384
Diogo Cunha <diogo.f.cunha@gmail.com>
2017-02-14 15:23:46 +0100

    new method with Module._load override

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var Module = require('module');
 var parent = require('parent-package-json');
 
 var parentPackageJson = parent(__dirname);
@@ -8,19 +9,32 @@ if (!parentPackageJson.path) {
   process.exit(0);
 }
 
-var manifestPath = process.env.CSS_MANIFEST || parentPackageJson.parse().cssManifest;
-if (!manifestPath) {
+var cssManifest = parentPackageJson.parse().cssManifest;
+if (!cssManifest || !cssManifest.path) {
   console.log('CSS manifest file is missing. Please set CSS_JSON_MANIFEST env variable or `cssManifest` property in package.json');
   process.exit(0);
 }
 
-var rootPath = path.dirname(parentPackageJson.path);
-var manifestAbsolutePath = path.join(rootPath, manifestPath);
-var manifest = require(manifestAbsolutePath);
+var rootDir = path.dirname(parentPackageJson.path);
+var manifestPath = path.join(rootDir, cssManifest.path);
+var base = cssManifest.base ? path.join(rootDir, cssManifest.base) : rootDir;
 
-var existingHook = require.extensions['.css'];
-require.extensions['.css'] = function cssManifestHook(m, filename) {
-  var key = path.relative(rootPath, filename);
-  var obj = manifest[key];
-  return m._compile(`module.exports = ${JSON.stringify(obj)}`, filename);
-}
+var manifest = require(manifestPath);
+
+var originalLoader = Module._load;
+
+Module._load = function(request, parent) {
+  if (!request.endsWith('.css')) {
+    return originalLoader.apply(this, arguments);
+  }
+
+  var dirname = path.dirname(parent.filename);
+  var filename = path.join(dirname, request);
+  var key = path.relative(base, filename);
+
+  if (!manifest[key]) {
+    return originalLoader.apply(this, arguments);
+  }
+
+  return manifest[key];
+};
